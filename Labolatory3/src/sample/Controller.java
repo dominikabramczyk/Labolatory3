787303814c6bf6a558d061d0e5fa08f3b1b7e268
dominikabramczyk <dominik.abramczyk.1@gmail.com>
2021-06-09 18:54:38 +0200

    Successful

--- Labolatory3/src/sample/Controller.java
+++ Labolatory3/src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 
 public class Controller {
+
+    public Button btnMain;
+    public TextField txtLicznik;
+    int clickCounter = 0;
+
+    public void onBtnAction(ActionEvent actionEvent) {
+        System.out.println("Kliknięcie nr " + ++clickCounter);
+        if (clickCounter == 1)
+            btnMain.setText("I jeszcze raz");
+        txtLicznik.setText(Integer.toString(clickCounter));
+    }
 }
